8a591738705cd8c96d51b760a83a1401ba67669a
Deminell <Deminell@yandex.ru>
2022-11-29 11:19:03 +0300

    new function added

--- Functions/Declare.go
+++ Functions/Declare.go
@@ -1,7 +1,14 @@
 package functions
 
-func MyFunc(x, y int) (half int) {
+func Multi(x, y int) (half int) {
 	half = x * y
+	return
+}
+
+func SumVar(x ...int) (res int) {
+	for _, v := range x {
+		res += v
+	}
 	return
 }
 
